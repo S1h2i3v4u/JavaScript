27c06a5d4bfd5e763efc5116a078b212da525b42
Shivani Suryawansi <158851522+S1h2i3v4u@users.noreply.github.com>
2025-07-22 19:41:28 +0530

    Create script.js

---
+++ Color-Flipper/script.js
@@ -0,0 +1,12 @@
+function setColor(color) {
+        document.body.style.backgroundColor = color;
+      }
+
+      function randomColor() {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+
+        const color = `rgb(${red}, ${green}, ${blue})`;
+        document.body.style.backgroundColor = color;
+      }
